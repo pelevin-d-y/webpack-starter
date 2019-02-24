77badcd5f9f8e991f5cf8e3e55a9388a4f0f41bc
pelevin-d-y <natpuot1992@gmail.com>
2019-02-24 12:43:02 +0300

    canvas paint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,26 @@
 import '../styles/index.scss';
 
+var canvas = document.getElementById('c1');
+var ctx = canvas.getContext('2d');
+var myColor = 'red';
+console.log('myColor', myColor)
 
-console.log('webpack starterkit', $('h1'));
+document.getElementById('color').oninput = function(){
+	myColor = this.value;
+}
+
+
+	
+canvas.onmousedown = function(){
+	canvas.onmousemove = function(event){
+		var x = event.offsetX;
+    var y = event.offsetY;
+    console.log(y, x)
+		ctx.fillStyle = myColor;
+		ctx.fillRect(x-5, y-5, 10, 10);
+		ctx.fill();
+	}
+	canvas.onmouseup = function(){
+		canvas.onmousemove = null;
+	}
+}
